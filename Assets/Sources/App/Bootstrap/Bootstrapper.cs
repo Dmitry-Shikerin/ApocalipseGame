ffcc5e8aa512b6f7449c863649a439cf0f457528
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-03-10 16:25:59 +0300

    add UniTask's in project

--- Assets/Sources/App/Bootstrap/Bootstrapper.cs
+++ Assets/Sources/App/Bootstrap/Bootstrapper.cs
@@ -1,17 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Bootstrapper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
         
     }
---
+++ Assets/Sources/App/Core/AppCore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sources.App.Core
+{
+    public class AppCore : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void LateUpdate()
+        {
+            
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void Construct()
+        {
+            
+        }
+    }
+}
