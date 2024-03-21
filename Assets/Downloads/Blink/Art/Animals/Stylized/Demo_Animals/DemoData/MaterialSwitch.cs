4de4ab8da987e152fef1ed6ecec92d900b5fb449
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-03-21 19:00:36 +0300

    add bear asset

---
+++ Assets/Downloads/Blink/Art/Animals/Stylized/Demo_Animals/DemoData/MaterialSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwitch : MonoBehaviour
+{
+    public Material[] Materials;
+    public Renderer Renderer;
+    private int matIndex= -1;
+
+    public float delay = 2;
+    private float nextMaterial;
+
+    private bool ready;
+    public AudioSource audio;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ready = true;
+            audio.Play();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!ready) return;
+        if (Time.time >= nextMaterial)
+        {
+            nextMaterial = Time.time + delay;
+
+            matIndex++;
+            if (Materials.Length - 1 < matIndex) matIndex = 0;
+            Renderer.material = Materials[matIndex];
+        }
+    }
+}
